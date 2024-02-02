9e630045dca8b9432e1b028f874b682f58b5c78e
Lyubov Statsenko <stlarek10@gmail.com>
2024-02-02 11:41:10 +0600

    added third task

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
-﻿// int number = 453; 
-// int firstDigit = number / 100;
-// int thirdDigit = number % 10; 
-// int result = firstDigit * 10 + thirdDigit;
-// Console.WriteLine($"В числе {number} => {result}");
+﻿// // int number = 453; 
+// // int firstDigit = number / 100;
+// // int thirdDigit = number % 10; 
+// // int result = firstDigit * 10 + thirdDigit;
+// // Console.WriteLine($"В числе {number} => {result}");
 
 
-int number = 947;
-int thirdDigit = number % 10;
-int secondDigit = number / 10 % 10; 
-int result = (int)Math.Pow(secondDigit, thirdDigit);
-Console.WriteLine($"{secondDigit} ^ {thirdDigit} = {result}");
+// int number = 947;
+// int thirdDigit = number % 10;
+// int secondDigit = number / 10 % 10; 
+// int result = (int)Math.Pow(secondDigit, thirdDigit);
+// Console.WriteLine($"{secondDigit} ^ {thirdDigit} = {result}");
+
+
+int number = 925254236; 
+if (number >= 100)
+{
+int thirdDigitFromEnd = number / 100 % 10; 
+Console.WriteLine($"Третья цифра от {number} с конца: {thirdDigitFromEnd}"); 
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
