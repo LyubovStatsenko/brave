f434f28bddc5682fc895f672423944d9ba2ebcd5
Lyubov Statsenko <stlarek10@gmail.com>
2024-02-02 11:23:09 +0600

    added first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = 453; 
+int firstDigit = number / 100;
+int thirdDigit = number % 10; 
+int result = firstDigit * 10 + thirdDigit;
+Console.WriteLine($"В числе {number} => {result}");
