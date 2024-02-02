e0900abda90575cbb721bbd61425ad823775745e
Lyubov Statsenko <stlarek10@gmail.com>
2024-02-02 11:31:27 +0600

    added second task

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
-﻿int number = 453; 
-int firstDigit = number / 100;
-int thirdDigit = number % 10; 
-int result = firstDigit * 10 + thirdDigit;
-Console.WriteLine($"В числе {number} => {result}");
+﻿// int number = 453; 
+// int firstDigit = number / 100;
+// int thirdDigit = number % 10; 
+// int result = firstDigit * 10 + thirdDigit;
+// Console.WriteLine($"В числе {number} => {result}");
+
+
+int number = 947;
+int thirdDigit = number % 10;
+int secondDigit = number / 10 % 10; 
+int result = (int)Math.Pow(secondDigit, thirdDigit);
+Console.WriteLine($"{secondDigit} ^ {thirdDigit} = {result}");
